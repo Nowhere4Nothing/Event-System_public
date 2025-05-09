e6fb2118dc0bddfa02a2537e52938aa935703fc4
Nowhere4Nothing <wmahler03@gmail.com>
2025-05-09 11:01:03 +1000

    Update Nav

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,17 @@
+import React, { useState, useEffect } from 'react';
 import './Navbar.css';
 
 function Navbar() {
+    const [events, setEvents] = useState([]);
+    const [searchTerm, setSearchTerm] = useState('');
+
+    useEffect(() => {
+        fetch('http://localhost:5000/events/')
+            .then(res => res.json())
+            .then(data => setEvents(data))
+            .catch(err => console.log('Error fetching events',err));
+    })
+
   return (
     <nav className="navbar">
       <div className="logo">Eventual</div>
