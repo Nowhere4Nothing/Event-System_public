5bd8ba68ba9e5985410d8fcfbc9721423959d1ac
Nowhere4Nothing <wmahler03@gmail.com>
2025-05-12 11:35:23 +1000

    fixing the react dom issue
    Downgraded the React dom version to version 6.22.3

--- src/components/FeaturedEvents.test.js
+++ src/components/FeaturedEvents.test.js
@@ -1,8 +1,13 @@
 import { render, screen } from '@testing-library/react';
 import FeaturedEvents from './FeaturedEvents';
 
+jest.mock('react-router-dom', () => ({
+    ...jest.requireActual('react-router-dom'), // keep other exports unchanged
+    useNavigate: () => jest.fn(), // mock useNavigate as a function
+}));
 
 describe('FeaturedEvents', () => {
+
     test('renders FeaturedEvents component', () => {
         render(<FeaturedEvents/>);
 
@@ -17,4 +22,7 @@ describe('FeaturedEvents', () => {
         render(<FeaturedEvents />);
 
     });
+
+
+
 });
